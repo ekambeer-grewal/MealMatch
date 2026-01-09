efdacf81b19695fb34f9f7aa59a2e31d40190ac5
Ekambeer Grewal <ekambeergrewal369@gmail.com>
2026-01-09 15:56:29 -0500

    Initial Commit

---
+++ Meal Match/Meal_MatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_MatchApp.swift
+//  Meal Match
+//
+//  Created by Ekambeer Grewal on 1/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Meal_MatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
